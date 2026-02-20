1c5ba31de131eaee81b62d177f2d1db8976e2b95
Muhammad Hilmy Abdul Aziz <108089955+Hilmy224@users.noreply.github.com>
2026-02-20 22:13:46 +0700

    leaderboard-temp

--- scenes/title_screen/title_screen.gd
+++ scenes/title_screen/title_screen.gd
@@ -6,6 +6,14 @@ func _ready():
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/title_screen/loading_screen.tscn")
 
+func _on_challenge_button_pressed():
+	# Start challenge mode and go straight to level 1
+	LevelProgression.start_challenge_mode()
+	get_tree().change_scene_to_file("res://scenes/title_screen/loading_screen.tscn")
+
+func _on_leaderboard_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/title_screen/leaderboard_screen.tscn")
+
 func _on_tutorial_button_pressed():
 	pass
 
